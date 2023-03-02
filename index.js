0710e4ee7dbf30bcb1cf090edd2ede7f73a84169
aloycwl <63489563+aloycwl@users.noreply.github.com>
2023-03-02 17:01:09 +0800

    2 files modified

--- index.js
+++ index.js
@@ -76,7 +76,8 @@ function game(x) {
   Unlock for withdrawal function
 */
 function unlock() {
-  $('#btn').prop('disabled', Number($('#amt').val()) > 50 ? false : true);
+  $('#ttf').val('Transfer 转出');
+  $('#ttf').prop('disabled', Number($('#amt').val()) > 50 ? false : true);
 }
 
 /*
@@ -96,7 +97,7 @@ async function transfer() {
       fromPrivateKey: KEY,
     }),
   });
-  $('pre').append(JSON.stringify(JSON.parse(await resp.text()), null, '\t'));
+  $('#ttf').val('Transferred 已转出');
   $('#amt').val(0);
   game(0);
 }
