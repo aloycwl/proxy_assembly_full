2fced922d6bbb561d36b259b7f688df226fe2f9e
aloycwl <63489563+aloycwl@users.noreply.github.com>
2023-03-02 09:48:17 +0800

    1 file modified

--- tatumGenerate.js
+++ tatumGenerate.js
@@ -1,12 +1,12 @@
-const resp = async () => {
-  await fetch(`https://api.tatum.io/v3/bsc/wallet`, {
+async function a() {
+  const resp = await fetch(`https://api.tatum.io/v3/bsc/wallet`, {
     method: 'GET',
     headers: {
       'x-api-key': 'f1384f0e-abd1-4d69-bb64-4682beb7fde4',
     },
   });
-};
-const data = async () => {
-  await resp.text();
-};
-console.log(data);
+
+  const data = await resp.text();
+  console.log(data);
+}
+a();
