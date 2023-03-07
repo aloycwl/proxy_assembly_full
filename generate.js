3e7205b56cda01e7975bbab7b10e62ef79d95870
aloycwl <63489563+aloycwl@users.noreply.github.com>
2023-03-07 11:30:29 +0800

    5 files deleted

--- generate.js
+++
@@ -1,6 +0,0 @@
-const EthereumWallet = require('ethereumjs-wallet');
-
-const wallet = EthereumWallet['default'].generate();
-
-console.log('address: ' + wallet.getAddressString());
-console.log('privateKey: ' + wallet.getPrivateKeyString());
