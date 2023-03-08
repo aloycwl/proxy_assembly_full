647d26af550a283defcb63446df37bde83d4bf61
aloycwl <63489563+aloycwl@users.noreply.github.com>
2023-03-08 14:18:55 +0800

    1 file modified

---
+++ API.js
@@ -0,0 +1,22 @@
+/*
+Initialise
+*/
+API = {
+  'Content-Type': 'application/json',
+  'x-api-key': 'f1384f0e-abd1-4d69-bb64-4682beb7fde4',
+};
+
+$('#btnBSC').on('click', async function (event) {
+  const web3 = new Web3(window.ethereum);
+  key = web3.eth.accounts.privateKeyToAccount($('#txtKey').val());
+  const resp = await fetch(
+    `https://api.tatum.io/v3/bsc/account/balance/${key.address}`,
+    {
+      method: 'GET',
+      headers: API,
+    }
+  );
+
+  const data = await resp.text();
+  $('#lblBSC').html(data);
+});
