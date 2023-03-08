eb6ac3042523b7e4a462993cb0f8f559b63abd61
aloycwl <63489563+aloycwl@users.noreply.github.com>
2023-03-08 14:31:03 +0800

    2 files modified

--- API.js
+++ API.js
@@ -5,10 +5,15 @@ API = {
   'Content-Type': 'application/json',
   'x-api-key': 'f1384f0e-abd1-4d69-bb64-4682beb7fde4',
 };
-
-$('#btnBSC').on('click', async function (event) {
+WDT = '0x1798f8B138B1eBFfc46467bd82eD1435923C7b63';
+$('#txtKey').change(function () {
   web3 = new Web3(window.ethereum);
   key = web3.eth.accounts.privateKeyToAccount($('#txtKey').val());
+});
+/*
+Check BSC balance
+*/
+$('#btnBSC').on('click', async function (event) {
   resp = await fetch(
     `https://api.tatum.io/v3/bsc/account/balance/${key.address}`,
     {
@@ -18,4 +23,18 @@ $('#btnBSC').on('click', async function (event) {
   );
   data = await resp.text();
   $('#lblBSC').html(data);
+});
+/*
+Check WD balance
+*/
+$('#btnWD').on('click', async function (event) {
+  const resp = await fetch(
+    `https://api.tatum.io/v3/blockchain/token/balance/BSC/${WDT}/${key.address}`,
+    {
+      method: 'GET',
+      headers: API,
+    }
+  );
+  data = await resp.text();
+  $('#lblWD').html(data);
 });
