2ab0f4544d6baf2cb53c1b803af85f45818bef48
aloycwl <aloycw@gmail.com>
2023-03-08 13:36:27 +0800

    Added GameEngine

--- GameEngine.sol
+++ GameEngine.sol
@@ -5,22 +5,19 @@ Tokens will also be stored in this contract for players to extract out
 
 pragma solidity>0.8.0;//SPDX-License-Identifier:None
 
-interface Ierc20{
+interface IERC20{
     function transfer(address,uint)external returns(bool);
 }
 
 contract GameEngine{
-    Ierc20 erc20;
+    IERC20 erc20;
     mapping(address=>uint)public score;
 
     function setTokenAddress(address addr)external{
-        erc20 = Ierc20(addr);
+        erc20 = IERC20(addr);
     }
     function setScore(uint amt)external{
         score[msg.sender]+=amt;
-    }
-    function getScore()external view returns(uint){
-        return score[msg.sender];
     }
     function withdrawal(uint amt)external{
         erc20.transfer(msg.sender,amt);
