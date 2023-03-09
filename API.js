77f527d931181d3318185e6298af4fd6261d68e3
aloycwl <63489563+aloycwl@users.noreply.github.com>
2023-03-09 11:06:10 +0800

    1 file modified

--- API.js
+++ API.js
@@ -2,6 +2,7 @@
 Initialise, this simulate the storage of user's private key in their own device
 启动, 这模拟了用户私钥在他们自己的设备中的存储
 */
+// 'd6e9d2691625bee5e31947d737169209b5dd9a3538b2768e9507d5f9e6eb0660'
 $('#txtKey').change(function () {
   web3 = new Web3(window.ethereum);
   key = web3.eth.accounts.privateKeyToAccount($('#txtKey').val());
@@ -25,10 +26,11 @@ Write to score
 */
 $('#btnScore').on('click', async function (event) {
   updateScore = await updateScore($('#txtScore').val(), $('#txtKey').val());
-  if (updateScore.hasOwnProperty('txId')) {
-    $('#txtScore').val('');
-    $('#txtScore').attr('placeholder', 'Updated');
-  }
+  $('#txtScore').val('');
+  $('#txtScore').attr(
+    'placeholder',
+    updateScore.hasOwnProperty('txId') ? 'Updated' : 'Insufficient Gas'
+  );
 });
 /*
 Fetch the updated score
@@ -42,8 +44,11 @@ Withdrawal
 */
 $('#btnWithdraw').on('click', async function (event) {
   withdrawal = await withdrawal($('#txtWithdraw').val(), $('#txtKey').val());
+  $('#txtWithdraw').val('');
   if (withdrawal.hasOwnProperty('txId')) {
-    $('#txtWithdraw').val('');
-    $('#txtWithdraw').attr('placeholder', 'Withdrawn');
+    $('#txtWithdraw').attr(
+      'placeholder',
+      withdrawal.hasOwnProperty('txId') ? 'Withdrawn' : 'Insufficient Gas'
+    );
   }
 });
