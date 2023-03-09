52b661c56df4568d8c2779c1e726e7a83ec3bf1c
aloycwl <63489563+aloycwl@users.noreply.github.com>
2023-03-09 13:41:04 +0800

    1 file modified

--- API.js
+++ API.js
@@ -9,46 +9,27 @@ $('#txtKey').change(function () {
   key = web3.eth.accounts.privateKeyToAccount($('#txtKey').val());
 });
 
-/*
-Check BSC balance
-*/
 $('#btnBSC').on('click', async function (event) {
   bsc = await balanceBSC(key.address);
   $('#lblBSC').html(bsc.balance);
 });
 
-/*
-Check WD balance
-*/
 $('#btnWD').on('click', async function (event) {
   wdt = await balanceWDT(key.address);
   $('#lblWD').html(wdt.balance);
 });
 
-/*
-Write to score
-*/
 $('#btnScore').on('click', async function (event) {
   _score = await updateScore($('#txtScore').val(), $('#txtKey').val());
   $('#btnScore').html(_score.hasOwnProperty('txId') ? 'Updated' : 'Error');
 });
 
-/*
-Fetch the updated score
-*/
 $('#btnCheckScore').on('click', async function (event) {
   _new_score = await getScore(key.address);
   $('#txtCheckScore').html(_new_score.data);
 });
 
-/*
-Withdrawal
-*/
 $('#btnWithdraw').on('click', async function (event) {
-  withdrawal = await withdrawal($('#txtWithdraw').val(), $('#txtKey').val());
-  $('#txtWithdraw').val('');
-  $('#txtWithdraw').attr(
-    'placeholder',
-    withdrawal.hasOwnProperty('txId') ? 'Withdrawn' : 'Insufficient Gas'
-  );
+  _out = await withdrawal($('#txtWithdraw').val(), $('#txtKey').val());
+  $('#btnWithdraw').html(_out.hasOwnProperty('txId') ? 'Withdrawn' : 'Error');
 });
