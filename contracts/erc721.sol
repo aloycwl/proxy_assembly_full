adff5e175665a9bcccbd1fc6cfd036ba1aa0f3ec
aloycwl <aloycw@gmail.com>
2023-03-13 09:40:32 +0800

    Cleaning up

--- contracts/erc721.sol
+++ contracts/erc721.sol
@@ -50,11 +50,11 @@ contract ERC721AC is IERC721,IERC721Metadata{
         return itf==type(IERC721).interfaceId||itf==type(IERC721Metadata).interfaceId;
     }
     function balanceOf(address addr)external view returns(uint){
-        require(!u[addr].blocked,"Address is blocked");
+        require(!u[addr].blocked,"Suspended");
         return u[addr].bal;
     }
     function ownerOf(uint id)external view returns(address){
-        require(!u[_owners[id]].blocked,"Address is blocked");
+        require(!u[_owners[id]].blocked,"Suspended");
         return _owners[id]; 
     }
     function owner()external view returns(address){
@@ -87,8 +87,7 @@ contract ERC721AC is IERC721,IERC721Metadata{
     function transferFrom(address from,address to,uint id)public{unchecked{
         require(msg.sender==_owners[id]||getApproved(id)==from||isApprovedForAll(_owners[id],from)||msg.sender==_owner
             ,"Invalid ownership");
-        require(!Suspended,"Contract is currently suspended");
-        require(!u[from].blocked,"Address is blocked");
+        require(!Suspended&&!u[from].blocked,"Suspended");
         (_tokenApprovals[id]=address(0),u[from].bal--,u[to].bal++,_owners[id]=to);
         emit Approval(_owners[id],to,id);
         emit Transfer(from,to,id);
@@ -104,10 +103,10 @@ contract ERC721AC is IERC721,IERC721Metadata{
     Custom functions
     自定函数
     */
-    function getOwned(address from)external view returns(uint[]memory _ids){
-        require(!u[from].blocked,"Address is blocked");
-        _ids=new uint[](u[from].bal);
-        for(uint i=0;i<u[from].bal;i++)_ids[i]=u[from].nfts[i];
+    function getTokensOwned(address addr)external view returns(uint[]memory _ids){
+        require(!u[addr].blocked,"Suspended");
+        _ids=new uint[](u[addr].bal);
+        for(uint i=0;i<u[addr].bal;i++)_ids[i]=u[addr].nfts[i];
     }
     function toString(uint _i)private pure returns(bytes memory bstr){unchecked{
         if(_i==0)return"0";
@@ -130,7 +129,7 @@ contract ERC721AC is IERC721,IERC721Metadata{
         delete _owners[id];
     }}
     function Mint()external{unchecked{
-        require(!u[msg.sender].blocked,"Address is blocked");
+        require(!u[msg.sender].blocked,"Suspended");
         (_owners[Count],u[msg.sender].nfts[u[msg.sender].bal])=(msg.sender,Count);
         (Count++,u[msg.sender].bal++);
         emit Transfer(address(this),msg.sender,Count);
