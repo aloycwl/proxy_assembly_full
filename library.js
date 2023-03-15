db5901d48e96a4239579d1d2d1fa6d76c7a030fd
aloycwl <63489563+aloycwl@users.noreply.github.com>
2023-03-15 15:18:29 +0800

    1 file modified

--- library.js
+++ library.js
@@ -2,70 +2,53 @@ API = {
   'Content-Type': 'application/json',
   'x-api-key': 'f1384f0e-abd1-4d69-bb64-4682beb7fde4',
 };
+API2 = {
+  method: 'GET',
+  headers: API,
+};
 WDT = '0x0C3FeE0988572C2703F1F5f8A05D1d4BFfeFEd5D';
 CONTRACT_GAME = '0xd511E66bCB935302662f49211E0281a5878A4F92';
 /*
 Below are the wallet functions
 以下都是钱包功能
 */
+async function fetchJson(url, options) {
+  return JSON.parse(await (await fetch(url, options)).text());
+}
 async function walletGenerate() {
-  return JSON.parse(
-    await (
-      await fetch(`https://api.tatum.io/v3/bsc/wallet`, {
-        method: 'GET',
-        headers: API,
-      })
-    ).text()
-  );
+  return fetchJson(`https://api.tatum.io/v3/bsc/wallet`, API2);
 }
 async function walletAddress(_addr) {
-  return JSON.parse(
-    await (
-      await fetch(`https://api.tatum.io/v3/bsc/address/${_addr}/1`, {
-        method: 'GET',
-        headers: API,
-      })
-    ).text()
-  );
+  return fetchJson(`https://api.tatum.io/v3/bsc/address/${_addr}/1`, API2);
 }
 async function walletPKey(_mne) {
-  return await (
-    await fetch(`https://api.tatum.io/v3/bsc/wallet/priv`, {
-      method: 'POST',
-      headers: API,
-      body: JSON.stringify({
-        index: 0,
-        mnemonic: _mne,
-      }),
-    })
-  ).json();
+  return fetchJson(`https://api.tatum.io/v3/bsc/wallet/priv`, {
+    method: 'POST',
+    headers: API,
+    body: JSON.stringify({
+      index: 0,
+      mnemonic: _mne,
+    }),
+  });
 }
 /*
 Check balance functions
 查余额功能
 */
 async function balanceBSC(_addr) {
-  j = JSON.parse(
-    await (
-      await fetch(`https://api.tatum.io/v3/bsc/account/balance/${_addr}`, {
-        method: 'GET',
-        headers: API,
-      })
-    ).text()
-  );
+  j = await fetchJson(`https://api.tatum.io/v3/bsc/account/balance/${_addr}`, {
+    method: 'GET',
+    headers: API,
+  });
   return j.balance;
 }
 async function balanceWDT(_addr) {
-  j = JSON.parse(
-    await (
-      await fetch(
-        `https://api.tatum.io/v3/blockchain/token/balance/BSC/${WDT}/${_addr}`,
-        {
-          method: 'GET',
-          headers: API,
-        }
-      )
-    ).text()
+  j = await fetchJson(
+    `https://api.tatum.io/v3/blockchain/token/balance/BSC/${WDT}/${_addr}`,
+    {
+      method: 'GET',
+      headers: API,
+    }
   );
   return Number(j.balance) / 1e18;
 }
