3e9bd68b3276cfbdb6197ab78edec439158a1ee9
aloycwl <aloycw@gmail.com>
2023-03-23 12:42:19 +0800

    1

--- contracts/GEProxy.sol
+++ contracts/GEProxy.sol
@@ -1,21 +1,23 @@
 pragma solidity 0.8.19;//SPDX-License-Identifier:None
 
 interface GE{
-    function setTokenAddress(address addr)external;
-    function setScore(uint amt)external;
-    function withdrawal(uint amt)external;
+    function setTokenAddress(address)external;
+    function setScore(address, uint)external;
+    function withdrawal(address, uint)external;
 }
 
 contract GameEngine{
     GE m;
     address private _owner;
-
     constructor(address a){
         (m, _owner) = (GE(a), msg.sender);
     }
-    mapping(address=>uint)public score;
-
     function setTokenAddress(address a)external{ m.setTokenAddress(a); }
-    function setScore(uint a)external{ m.setScore(a); }
-    function withdrawal(uint a)external{ m.withdrawal(a); }
+    function setScore(address a, uint b)external{ m.setScore(a, b); }
+    function withdrawal(address a, uint b)external{ m.withdrawal(a, b); }
+
+    function NewAddress(address a)external{
+        require(msg.sender == _owner);
+        m = GE(a);
+    }
 }
