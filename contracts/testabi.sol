be67c584d6845c94272129c0db0c4f54f7ed2a6c
aloycwl <aloycw@gmail.com>
2023-04-04 10:52:56 +0800

    1

---
+++ contracts/testabi.sol
@@ -0,0 +1,15 @@
+pragma solidity 0.8.19;//SPDX-License-Identifier:None
+
+
+
+contract a{
+    mapping(uint => bytes) private aa;
+
+    function setAA(string calldata b, uint c)external{
+        aa[c] = abi.encode(b);
+    }
+
+    function getAA(uint c)external view returns (string memory) {
+        return abi.decode(aa[c], (string));
+    }
+}
