8d8651399328fb7cf91c109199336fa84fe04c29
aloycwl <aloycwl@gmail.com>
2023-04-07 09:46:40 +0800

    1

--- contracts/testECRecover.sol
+++ contracts/testECRecover.sol
@@ -9,9 +9,7 @@ contract VerifySignature {
     }
 
     function verify(string memory message, uint8 v, bytes32 r, bytes32 s) public view returns (bool) {
-        bytes32 messageHash = keccak256(abi.encodePacked(message));
-        bytes32 ethSignedMessageHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash));
-        address recoveredSigner = ecrecover(ethSignedMessageHash, v, r, s);
-        return (recoveredSigner == trustedSigner);
+        return (ecrecover(keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", 
+            keccak256(abi.encodePacked(message)))), v, r, s) == trustedSigner);
     }
 }
