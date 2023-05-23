92a127c8b5f9d978dc78f5f2e3be2ad3b07993a0
aloycwl <aloycw@gmail.com>
2023-05-23 11:14:40 +0800

    added comments

--- contracts/Proxy.sol
+++ contracts/Proxy.sol
@@ -5,8 +5,10 @@ import "./Util.sol";
 
 //代理合同
 contract Proxy is Util {
+
     mapping (uint => address) public addrs;
 
+    //动态设置要索引的地址
     function setAddr(address addr, uint index) external OnlyAccess() {
         addrs[index] = addr;
     }
