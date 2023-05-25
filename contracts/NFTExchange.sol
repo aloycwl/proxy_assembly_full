63f84edd74dc2bbc4bfa8d1118d5345f9dad3abb
aloycwl <aloycw@gmail.com>
2023-05-25 11:36:20 +0800

    Updated sell, remove, buy

--- contracts/NFTExchange.sol
+++ contracts/NFTExchange.sol
@@ -12,45 +12,65 @@ contract agora is Util {
         uint price;
     }
 
-    uint public listed;
+    address private owner;
+    uint private counter;
     uint public fee = 0; //小数点后两位的百分比，xxx.xx 
-    address private _owner;
+    uint[] public arrList;
     mapping(uint => List) public list;
 
     constructor() {
-        _owner = msg.sender;
+        owner = msg.sender;
     }
 
-    function Sell(address _nftAdd, uint _tokenId, uint _price) external {
+    //卖功能，需要先设置NFT合约的认可
+    function sell(address contractAddr, uint tokenId, uint price) external {
         unchecked {
-            require(IERC721(_nftAdd).getApproved(_tokenId) == address(this));
-            require(IERC721(_nftAdd).ownerOf(_tokenId) == msg.sender);
-            List storage l = list[listed];
-            (l.contractAddr, l.tokenId, l.price) = (_nftAdd, _tokenId, _price);
-            ++listed;
+            assert(IERC721(contractAddr).getApproved(tokenId) == address(this));
+            
+            List storage l = list[counter];
+            (l.contractAddr, l.tokenId, l.price) = (contractAddr, tokenId, price);
+            arrList.push(++counter);
         }
     }
 
-    function Buy(uint _id) external payable {
-        unchecked {
-            List storage l = list[_id];
-            uint _price = l.price;
-            require(msg.value >= _price);
-            address seller = IERC721(l.contractAddr).ownerOf(l.tokenId);
-            IERC721(l.contractAddr).transferFrom(seller, address(this), l.tokenId);
-            IERC721(l.contractAddr).transferFrom(address(this), msg.sender, l.tokenId);
-            payable(seller).transfer(_price * (10000 - fee / 10000));
-            payable(_owner).transfer(address(this).balance);
-            delete list[_id];
+    //取消列表功能，也将在成功购买时调用
+    function remove(uint id) public {
+        unchecked{
+            uint count = arrList.length;
+            for (uint i; i < count; ++i)
+                if (arrList[i] == id) {
+                    arrList[i] = arrList[count - 1];
+                    arrList.pop();
+                    break;
+                }
+            delete list[id];
         }
     }
 
-    function Show(uint batch,  uint offset) external view returns
+    //用户必须发送大于或等于所列价格的以太币
+    function buy(uint id) external payable {
+        unchecked {
+            List storage l = list[id];
+            uint tokenId = l.tokenId;
+            uint price = l.price;
+            assert(msg.value >= price);
+
+            address seller = IERC721(l.contractAddr).ownerOf(tokenId);
+            IERC721(l.contractAddr).transferFrom(seller, address(this), tokenId);
+            IERC721(l.contractAddr).transferFrom(address(this), msg.sender, tokenId);
+
+            payable(seller).transfer(price * (10000 - fee / 10000));
+            payable(owner).transfer(address(this).balance);
+            remove(id);
+        }
+    }
+
+    function display(uint batch,  uint offset) external view returns
         (string[]memory tu, uint[]memory price, uint[]memory listId) {
-            unchecked{
+        unchecked{
             (tu, price, listId) = (new string[](batch), new uint[](batch), new uint[](batch));
             uint b;
-            uint i = listed - offset;
+            uint i = arrList.length - offset;
             while(b < batch && i > 0) {
                 List storage l = list[--i];
                 if(IERC721(l.contractAddr).getApproved(l.tokenId) == address(this)) {
@@ -62,9 +82,7 @@ contract agora is Util {
         }
     }
 
-    //get individual listing information
-
-    //delist the item
+    //行政功能
 
     function setFee(uint amt) external OnlyAccess {
         fee = amt;
