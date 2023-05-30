199feaa5f694309eecb8d115bf3f3a54889b2ae5
aloycwl <aloycwl@gmail.com>
2023-05-30 10:35:16 +0800

    Simplified listing mapping

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -6,16 +6,10 @@ import "./Util.sol";
 
 contract NFTMarket is Util {
 
-    struct List {
-        address contractAddr;
-        uint tokenId;
-        uint price;
-    }
-
     address private wallet;
     uint private counter;
     uint public fee = 0; //小数点后两位的百分比，xxx.xx
-    mapping(uint => List) public list;
+    mapping(address => mapping(uint => uint)) public list;
 
     constructor() {
         wallet = msg.sender;
@@ -23,35 +17,29 @@ contract NFTMarket is Util {
 
     //卖功能，需要先设置NFT合约的认可
     function sell(address contractAddr, uint tokenId, uint price) external {
-        unchecked {
-            require(IERC721(contractAddr).getApproved(tokenId) == address(this), "No approval");
-            
-            List storage l = list[counter];
-            (l.contractAddr, l.tokenId, l.price) = (contractAddr, tokenId, price);
-        }
+        require(IERC721(contractAddr).getApproved(tokenId) == address(this), "No approval");
+        list[contractAddr][tokenId] = price;
     }
 
     //取消列表功能，也将在成功购买时调用
-    function remove(uint id) public {
-        List storage _list = list[id];
-        require(IERC721(_list.contractAddr).ownerOf(_list.tokenId) == msg.sender, "Not owner");
-        delete list[id];
+    function remove(address contractAddr, uint tokenId) public {
+        require(IERC721(contractAddr).ownerOf(tokenId) == msg.sender, "Not owner");
+        delete list[contractAddr][tokenId];
     }
 
     //用户必须发送大于或等于所列价格的以太币
-    function buy(uint id) external payable {
+    function buy(address contractAddr, uint tokenId) external payable {
         unchecked {
-            List storage l = list[id];
-            (uint tokenId, uint price) = (l.tokenId, l.price);
+            uint price = list[contractAddr][tokenId];
             require(msg.value >= price, "Insufficient price");
 
-            address seller = IERC721(l.contractAddr).ownerOf(tokenId);
-            IERC721(l.contractAddr).transferFrom(seller, address(this), tokenId);
-            IERC721(l.contractAddr).transferFrom(address(this), msg.sender, tokenId);
+            address seller = IERC721(contractAddr).ownerOf(tokenId);
+            IERC721(contractAddr).transferFrom(seller, address(this), tokenId);
+            IERC721(contractAddr).transferFrom(address(this), msg.sender, tokenId);
 
             payable(seller).transfer(price * (10000 - fee / 10000));
             payable(wallet).transfer(address(this).balance);
-            remove(id);
+            remove(contractAddr, tokenId);
         }
     }
 
