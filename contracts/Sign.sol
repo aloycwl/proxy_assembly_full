7ac3aeb87b0775fdb13ee97e1e261d7a00f2399f
aloycwl <aloycwl@gmail.com>
2023-06-05 15:52:45 +0800

    formatted space

--- contracts/Sign.sol
+++ contracts/Sign.sol
@@ -4,18 +4,23 @@ pragma solidity 0.8.18;
 import "./Lib.sol";
 import "./Interfaces.sol";
 
-contract Sign{
+contract Sign {
+
     IProxy private iProxy;
 
     constructor(address proxy) {
+
         iProxy = IProxy(proxy);
+
     }
 
     function check(address addr, uint8 v, bytes32 r, bytes32 s) internal {
+
         IDID iDID = IDID(iProxy.addrs(3));                  //签名者用3号索引
         uint counter = iDID.uintData(addr, 1);
         
         unchecked {
+
             require(
                 ecrecover(                                  //7. 还原
                     keccak256(                              //6. 再散列和编码
@@ -43,6 +48,9 @@ contract Sign{
             //更新计数器以防止类似的散列，并更新最后的时间戳
             iDID.updateUint(addr, 1, counter);
             iDID.updateUint(addr, 2, block.timestamp);
+
         }
+
     }
+    
 }
