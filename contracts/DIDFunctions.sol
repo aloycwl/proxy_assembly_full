b92608bebe056ae2f28a6c3a971336d18e61d4ba
aloycwl <aloycwl@gmail.com>
2023-06-05 15:58:00 +0800

    formatted space

--- contracts/DIDFunctions.sol
+++ contracts/DIDFunctions.sol
@@ -10,30 +10,38 @@ import "./Util.sol";
 import "./Interfaces.sol";
 
 contract DIDFunctions is Util {
+
     IDID iDID;
 
     //用户名不能重复
     modifier OnlyUnique(string calldata userName) {
+
         require(iDID.did(userName) == address(0), "Username existed");
         _;
+
     }
 
     constructor(address did){
+
         //调用主合约
         iDID = IDID(did);
+
     }
 
     //谁都可以创造新用户
     function createUser(address addr, string calldata userName, string calldata name, string calldata bio) 
         external OnlyUnique(userName) {
+
             iDID.updateDid(userName, addr);         //创建用户名
             iDID.updateString(addr, 0, userName);   //能够从地址中找到用户名
             iDID.updateString(addr, 1, name);       //添加名称
             iDID.updateString(addr, 2, bio);        //添加传记
+
     }
 
     //改用户名，删除旧名来省燃料
     function changeUsername(string calldata strBefore, string calldata strAfter) external OnlyUnique(strAfter) {
+
         address addr = iDID.did(strBefore);
 
         assert(msg.sender == addr);                 //只有所有者可以更改他们的用户名
@@ -41,5 +49,7 @@ contract DIDFunctions is Util {
         iDID.updateDid(strBefore, address(0));      //删除旧用户名
         iDID.updateDid(strAfter, addr);             //添加新用户名
         iDID.updateString(addr, 0, strAfter);       //更新地址搜索
+
     }
+    
 }
