86b1ca6b1894d1eefe81fb6dd20c4fd61441aa01
aloycwl <aloycwl@gmail.com>
2023-06-05 15:53:43 +0800

    formatted space

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -12,24 +12,32 @@ contract NFTMarket is Util {
     mapping(address => mapping(uint => uint)) public list;
 
     constructor() {
+
         wallet = msg.sender;
+
     }
 
     //卖功能，需要先设置NFT合约的认可
     function sell(address contractAddr, uint tokenId, uint price) external {
+
         require(IERC721(contractAddr).getApproved(tokenId) == address(this), "No approval");
         list[contractAddr][tokenId] = price;
+
     }
 
     //取消列表功能，也将在成功购买时调用
     function remove(address contractAddr, uint tokenId) public {
+
         require(IERC721(contractAddr).ownerOf(tokenId) == msg.sender, "Not owner");
         delete list[contractAddr][tokenId];
+
     }
 
     //用户必须发送大于或等于所列价格的以太币
     function buy(address contractAddr, uint tokenId) external payable {
+
         unchecked {
+
             uint price = list[contractAddr][tokenId];
             require(msg.value >= price, "Insufficient price");
 
@@ -40,11 +48,16 @@ contract NFTMarket is Util {
             payable(seller).transfer(price * (10000 - fee / 10000));
             payable(wallet).transfer(address(this).balance);
             remove(contractAddr, tokenId);
+
         }
+
     }
 
     //设置费用
     function setFee(uint amt) external OnlyAccess {
+
         fee = amt;
+
     }
+    
 }
