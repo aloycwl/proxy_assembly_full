f30cb60466470ccc8e3e4b58814438db98abf360
aloycwl <aloycwl@gmail.com>
2023-06-05 16:01:57 +0800

    versioning

--- contracts/Interfaces.sol
+++ contracts/Interfaces.sol
@@ -56,5 +56,5 @@ interface IERC721Metadata {
     function name() external view returns (string memory);
     function symbol() external view returns (string memory);
     function tokenURI(uint) external view returns (string memory);
-    
+
 }
