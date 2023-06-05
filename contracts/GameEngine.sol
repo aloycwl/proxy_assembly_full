3aa7ab162eadec4b4693310947a46455209f7994
aloycwl <aloycwl@gmail.com>
2023-06-05 15:55:37 +0800

    formatted space

--- contracts/GameEngine.sol
+++ contracts/GameEngine.sol
@@ -8,17 +8,22 @@ import "./Sign.sol";
 
 //游戏引擎
 contract GameEngine is Util, Sign {
+
     IProxy private iProxy;
     uint public withdrawInterval = 60;  //以秒为单位的默认设置
 
     constructor(address proxy) Sign(proxy) {
+
         //调用交叉合约函数
         iProxy = IProxy(proxy);
+
     }
     
     //利用签名人来哈希信息
     function withdraw(address addr, uint amt, uint8 v, bytes32 r, bytes32 s) external {
+
         unchecked {
+
             IDID idid = IDID(iProxy.addrs(3));
 
             //确保账户不会被暂停、提款过早或签名错误
@@ -30,11 +35,16 @@ contract GameEngine is Util, Sign {
 
             //开始转移
             IERC20(iProxy.addrs(2)).transfer(addr, amt);
+
         }
+        
     }
 
     //设置取款间隔
     function setWithdrawInterval(uint _withdrawInterval) external OnlyAccess {
+
         withdrawInterval = _withdrawInterval;
+
     }
+
 }
