e351ca84d9192cf2bc952d154466536960da3934
aloycwl <aloycwl@gmail.com>
2023-06-05 13:10:53 +0800

    Signing & update to Sign.sol

--- contracts/GameEngine.sol
+++ contracts/GameEngine.sol
@@ -11,7 +11,7 @@ contract GameEngine is Util, Sign {
     IProxy private iProxy;
     uint public withdrawInterval = 60;  //以秒为单位的默认设置
 
-    constructor(address proxy) {
+    constructor(address proxy) Sign(proxy) {
         //调用交叉合约函数
         iProxy = IProxy(proxy);
     }
@@ -20,38 +20,13 @@ contract GameEngine is Util, Sign {
     function withdraw(address addr, uint amt, uint8 v, bytes32 r, bytes32 s) external {
         unchecked {
             IDID idid = IDID(iProxy.addrs(3));
-            uint counter = idid.uintData(addr, 1);
 
             //确保账户不会被暂停、提款过早或签名错误
             require(idid.uintData(addr, 0) == 0, "Account is suspended");
             require(idid.uintData(addr, 2) + withdrawInterval < block.timestamp, "Withdraw too soon");
-            require(
-                ecrecover(                                  //7. 还原
-                    keccak256(                              //6. 再散列和编码
-                        abi.encodePacked(                   //   与外部哈希对齐
-                            keccak256(                      //5. 首先散列和编码
-                                abi.encodePacked(           
-                                    string.concat(          //4. 合并字符串
-                                        Lib.uintToString(   //2. uint变string 
-                                            uint(
-                                                uint160(
-                                                    addr    //1. address变uint
-                                                )           //   address变string
-                                            )
-                                        ), 
-                                        Lib.uintToString(   //3. uint变string
-                                            counter
-                                        )
-                                    )
-                                )
-                            )
-                        )
-                    ) , v, r, s
-                ) == iProxy.addrs(4), "Invalid signature");
 
-            //更新计数器以防止类似的散列，并更新最后的时间戳
-            idid.updateUint(addr, 1, counter + 1);
-            idid.updateUint(addr, 2, block.timestamp);
+            //检查签名和更新指数
+            check(addr, v, r, s);
 
             //开始转移
             IERC20(iProxy.addrs(2)).transfer(addr, amt);
--- contracts/Sign.sol
+++ contracts/Sign.sol
@@ -5,33 +5,44 @@ import "./Lib.sol";
 import "./Interfaces.sol";
 
 contract Sign{
-    function check(address proxy, address addr, uint8 v, bytes32 r, bytes32 s) external view {
-        IProxy iProxy = IProxy(proxy);
-        IDID idid = IDID(iProxy.addrs(3));
-        uint counter = idid.uintData(addr, 1);
+    IProxy private iProxy;
 
-        require(
-            ecrecover(                                  //7. 还原
-                keccak256(                              //6. 再散列和编码
-                    abi.encodePacked(                   //   与外部哈希对齐
-                        keccak256(                      //5. 首先散列和编码
-                            abi.encodePacked(           
-                                string.concat(          //4. 合并字符串
-                                    Lib.uintToString(   //2. uint变string 
-                                        uint(
-                                            uint160(
-                                                addr    //1. address变uint
-                                            )           //   address变string
+    constructor(address proxy) {
+        iProxy = IProxy(proxy);
+    }
+
+    function check(address addr, uint8 v, bytes32 r, bytes32 s) internal {
+        IDID iDID = IDID(iProxy.addrs(3));                  //签名者用3号索引
+        uint counter = iDID.uintData(addr, 1);
+        
+        unchecked {
+            require(
+                ecrecover(                                  //7. 还原
+                    keccak256(                              //6. 再散列和编码
+                        abi.encodePacked(                   //   与外部哈希对齐
+                            keccak256(                      //5. 首先散列和编码
+                                abi.encodePacked(           
+                                    string.concat(          //4. 合并字符串
+                                        Lib.uintToString(   //2. uint变string 
+                                            uint(
+                                                uint160(
+                                                    addr    //1. address变uint
+                                                )           //   address变string
+                                            )
+                                        ), 
+                                        Lib.uintToString(   //3. uint变string
+                                            ++counter       //增量供以后使用
                                         )
-                                    ), 
-                                    Lib.uintToString(   //3. uint变string
-                                        counter
                                     )
                                 )
                             )
                         )
-                    )
-                ) , v, r, s
-            ) == iProxy.addrs(4), "Invalid signature");
+                    ) , v, r, s
+                ) == iProxy.addrs(4), "Invalid signature");
+            
+            //更新计数器以防止类似的散列，并更新最后的时间戳
+            iDID.updateUint(addr, 1, counter);
+            iDID.updateUint(addr, 2, block.timestamp);
+        }
     }
 }
