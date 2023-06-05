6abacc0a07b72f825854f40cf79a3db47bc30f97
aloycwl <aloycwl@gmail.com>
2023-06-05 15:54:52 +0800

    formatted space

--- contracts/Lib.sol
+++ contracts/Lib.sol
@@ -5,16 +5,26 @@ library Lib {
 
     //整数转移字符
     function uintToString(uint num) internal pure returns (string memory) {
+
         unchecked{
-            if (num == 0) return "0";                   //如果整数为 0，则不需要移位
+
+            //如果整数为 0，则不需要移位
+            if (num == 0) return "0";                   
             uint j = num;
             uint l;
-            while (j != 0) (++l, j /= 10);              //得到 10 的倍数的迭代
-            bytes memory bstr = new bytes(l);           //从整数创建单个数字
+
+            //得到 10 的倍数的迭代
+            while (j != 0) (++l, j /= 10);              
+
+            //从整数创建单个数字
+            bytes memory bstr = new bytes(l);           
             j = num;
-            while (j != 0) (bstr[--l] =                 //遍历所有 10 的倍数
-                bytes1(uint8(48 + j % 10)), j /= 10);   //转换为单个整数而不是字节
+
+            //遍历所有 10 的倍数，转换为单个整数而不是字节
+            while (j != 0) (bstr[--l] = bytes1(uint8(48 + j % 10)), j /= 10);
+
             return string(bstr);
+
         }
     }
 }
