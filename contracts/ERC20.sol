851d0d6bc7d6a390861db97a1532e1269b8f0905
aloycwl <aloycwl@gmail.com>
2023-06-05 15:56:33 +0800

    formatted space

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -6,6 +6,7 @@ import "./Interfaces.sol";
 
 //代币合约
 contract ERC20 is Util {
+
     //ERC20标准变量 
     event Transfer(address indexed from, address indexed to, uint value);
     event Approval(address indexed owner, address indexed spender, uint value);
@@ -22,23 +23,31 @@ contract ERC20 is Util {
 
     //ERC20标准函数 
     constructor(address proxy, address receiver, uint amt, string memory _name, string memory _sym) {
+
         //调用交叉合约函数
         (iProxy, name, symbol) = (IProxy(proxy), _name, _sym);
         //铸造给定地址的代币数量
         mint(amt, receiver);
+
     }
 
     function approve(address to, uint amt) external returns (bool) {
+
         emit Approval(msg.sender, to, allowance[msg.sender][to] = amt);
         return true;
+
     }
 
     function transfer(address to, uint amt) external returns (bool) {
+
         return transferFrom(msg.sender, to, amt);
+
     }
 
     function transferFrom(address from, address to, uint amt) public returns (bool) {
+
         unchecked {                                                     //使用assert而不是require来节省燃料
+
             assert(balanceOf[from] >= amt);                             //地址必须有足够的代币才能转账
             assert(from == msg.sender ||                                //必须是地址所有者或
                 allowance[from][to] >= amt);                            //接收者已获得授权
@@ -50,30 +59,43 @@ contract ERC20 is Util {
             (balanceOf[from] -= amt, balanceOf[to] += amt);             //开始转移
             emit Transfer(from, to, amt);                               //发出日志
             return true;
+
         }
+
     }
 
     //自定义函数
 
     //切换暂停
     function toggleSuspend() external OnlyAccess {
+
         suspended = suspended == 0 ? 1 : 0;
+
     }
 
     //铸币代币，只允许有访问权限的地址
     function mint(uint amt, address addr) public OnlyAccess {
+
         unchecked {
+
             (totalSupply += amt, balanceOf[addr] += amt);   //将数量添加到用户和总供应量
             emit Transfer(address(this), addr, amt);        //发出日志
+
         }
+
     }
 
     //烧毁代币，任何人都可以烧毁
     function burn(uint amt) external {
+
         unchecked {
+
             assert(balanceOf[msg.sender] >= amt);           //燃烧者必须有足够的代币
             transferFrom(msg.sender, address(0), amt);      //调用标准函数
             totalSupply -= amt;                             //减少总供应
+
         }
+
     }
+    
 }
