20bb293387aa58e57d84190f23a12d2537d04239
aloycwl <aloycwl@gmail.com>
2023-06-09 13:43:08 +0800

    repathed

--- Contracts/GameEngine.sol
+++ Contracts/GameEngine.sol
@@ -1,8 +1,8 @@
 //SPDX-License-Identifier:None
 pragma solidity 0.8.18;
 
-import "./Util/Access.sol";
-import "./Util/Sign.sol";
+import "/Contracts/Util/Access.sol";
+import "/Contracts/Util/Sign.sol";
 
 //游戏引擎
 contract GameEngine is Access, Sign {
