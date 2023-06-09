b04ee91a51f6b570f00684a0217d179e88fc6669
aloycwl <aloycwl@gmail.com>
2023-06-09 16:21:09 +0800

    created append & prepend

--- Contracts/Util/LibString.sol
+++ Contracts/Util/LibString.sol
@@ -3,5 +3,16 @@ pragma solidity 0.8.18;
 
 library LibString {
 
+    function append(string calldata a, string calldata b) internal pure returns(string memory) {
+
+        return string.concat(a, b);
+
+    }
+
+    function prepend(string calldata a, string calldata b) internal pure returns(string memory) {
+
+        return string.concat(b, a);
+
+    }
 
 }
