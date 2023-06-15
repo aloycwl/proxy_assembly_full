54bfdd92452a03697fe34895a28967a0a575d253
aloycwl <aloycwl@gmail.com>
2023-06-15 14:29:18 +0800

    Using assembly

--- Contracts/Future/test.sol
+++ Contracts/Future/test.sol
@@ -1,19 +1,48 @@
 //SPDX-License-Identifier:None
 pragma solidity 0.8.18;
 
-import {LibString} from "Contracts/Util/LibString.sol";
+import "Contracts/Util/LibUint.sol";
 
 contract Test {
 
-    using LibString for string;
+    using LibUint for uint;
 
-    function test() external pure returns (string memory) {
+    mapping(address => uint) public uintData;
 
-        string memory a = "a";
-        string memory b = "b";
+    function storeUint() external {
+    
 
-        return a.prepend(b);
-
+        uintData[0x0000000000000000000000000000000000000002] = 556;
     }
 
+    function addition(uint x, uint y) public pure returns (uint) {
+        
+        assembly {
+            
+            
+            mstore(0x0, add(x, y))
+            
+            return(0x0, 32)          
+            
+        }
+    
+    }
 }
+
+   /*function setUint(address addr, uint value) external {
+
+        address t;
+
+        assembly {
+            // Load the address of the current contract into `m := 0x40`
+            let m := mload(0x40)
+
+            // Load the `msg.sender` value into `sender := 0x44`
+            t := calldataload(44)
+        }
+
+            uintData[t] = value;
+
+    }*/
+
+//
