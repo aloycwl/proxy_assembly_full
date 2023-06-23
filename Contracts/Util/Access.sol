9047ac000797eeb04d5e34a645d07b42c848926f
aloycwl <aloycwl@gmail.com>
2023-06-23 14:28:43 +0800

    combining access and addrs

--- Contracts/Util/Access.sol
+++ Contracts/Util/Access.sol
@@ -4,19 +4,19 @@ pragma solidity ^0.8.0;
 //置对合约的访问
 contract Access {
 
-    mapping(address => uint) public access;
+    mapping(address => uint) public addrs;
 
     //立即授予创建者访问权限
     constructor () {
 
-        access[msg.sender] = 1e3;
+        addrs[msg.sender] = 1e3;
 
     }
 
     //用作函数的修饰符
     modifier OnlyAccess () {
 
-        require(access[msg.sender] > 0,         "Insufficient access");
+        require(addrs[msg.sender] > 0,          "Insufficient access");
         _;
 
     }
@@ -24,12 +24,12 @@ contract Access {
     //只可以管理权限币你小的人和授权比自己低的等级
     function setAccess (address addr, uint u) external OnlyAccess {
 
-        uint acc = access[msg.sender];
+        uint acc = addrs[msg.sender];
 
         //不能修改访问权限高于用户的地址和授予高于自己的访问权限
-        require(acc > access[addr] && acc > u,  "Invalid access");
+        require(acc > addrs[addr] && acc > u,   "Invalid access");
 
-        access[addr] = u;
+        addrs[addr] = u;
 
     }
     
