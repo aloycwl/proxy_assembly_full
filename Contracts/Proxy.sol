e97717494cd1abad33f928b775274102be6c863e
aloycwl <aloycwl@gmail.com>
2023-06-23 14:19:24 +0800

    renamed

--- Contracts/Proxy.sol
+++ Contracts/Proxy.sol
@@ -25,20 +25,20 @@ contract Proxy is Access {
 
 /*
 部署顺序：
-1.  Proxy
-2.  DID 
-3.  Proxy setAddr(DID, 3)
-4.  GameEngine
-5.  DID setAccess() 授权到 GameEngine
-6.  Proxy setAddr(GameEngine, 1)
-7.  ERC20
-8.  DID setAccess() 授权到 ERC20
-9.  Proxy setAddr(ERC20, 2)
-10. ERC20 mint() 铸币到 GameEngine
-11. ERC721
-12. DID setAccess() 授权到 ERC721
-13. Proxy setAddr(ERC721, 5)
-14. Proxy 加签名者 setAddr(signer, 4)
-15. NFTMarket
-16. DID setAccess() 授权到 NFTMarket
+Proxy
+Proxy   => setAddr      (signer, 4)
+DID 
+Proxy   => setAddr      (DID, 3)
+Engine
+Proxy   => setAddr      (Engine, 1)
+DID     => setAccess    (Engine, 100)
+ERC20
+Proxy   => setAddr      (ERC20, 2)
+DID     => setAccess    (ERC20, 100)
+ERC20   => mint         (Engine, 10000000000000000000000000000000000000000)
+ERC721
+Proxy   => setAddr      (ERC721, 5)
+DID     => setAccess    (ERC721, 100) 
+Market
+DID     => setAccess    (Market, 100) 
 */
