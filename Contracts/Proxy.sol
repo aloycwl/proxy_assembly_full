341607e1a8b877ab56cce50ed42a6f3b0ebbd4a0
aloycwl <aloycwl@gmail.com>
2023-06-23 14:20:30 +0800

    edited comments

--- Contracts/Proxy.sol
+++ Contracts/Proxy.sol
@@ -26,19 +26,19 @@ contract Proxy is Access {
 /*
 部署顺序：
 Proxy
-Proxy   => setAddr      (signer, 4)
+Proxy   => setAddr      (signer,    4)
 DID 
-Proxy   => setAddr      (DID, 3)
+Proxy   => setAddr      (DID,       3)
 Engine
-Proxy   => setAddr      (Engine, 1)
-DID     => setAccess    (Engine, 100)
+Proxy   => setAddr      (Engine,    1)
+DID     => setAccess    (Engine,    100)
 ERC20
-Proxy   => setAddr      (ERC20, 2)
-DID     => setAccess    (ERC20, 100)
-ERC20   => mint         (Engine, 10000000000000000000000000000000000000000)
+Proxy   => setAddr      (ERC20,     2)
+DID     => setAccess    (ERC20,     100)
+ERC20   => mint         (Engine,    10000000000000000000000000000000000000000)
 ERC721
-Proxy   => setAddr      (ERC721, 5)
-DID     => setAccess    (ERC721, 100) 
+Proxy   => setAddr      (ERC721,    5)
+DID     => setAccess    (ERC721,    100) 
 Market
-DID     => setAccess    (Market, 100) 
+DID     => setAccess    (Market,    100) 
 */
