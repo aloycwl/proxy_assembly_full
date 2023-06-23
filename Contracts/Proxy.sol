d98b937af3a644902911a4e369ae76cc2895c575
aloycwl <aloycwl@gmail.com>
2023-06-23 14:24:24 +0800

    combining access and address

--- Contracts/Proxy.sol
+++ Contracts/Proxy.sol
@@ -5,21 +5,6 @@ import "Contracts/Util/Access.sol";
 
 //代理合同
 contract Proxy is Access {
-
-    mapping (uint => address) public addrs;
-
-    constructor () {
-
-        addrs[0] = address(this);
-
-    }
-
-    //动态设置要索引的地址
-    function setAddr (address addr, uint index) external OnlyAccess () {
-
-        addrs[index] = addr;
-
-    }
     
 }
 
