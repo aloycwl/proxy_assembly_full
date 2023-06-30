4c5702874ab902756f99a6c874a51196300c97fa
aloycwl <aloycwl@gmail.com>
2023-06-30 15:04:30 +0800

    Updated import

--- Contracts/ERC721.sol
+++ Contracts/ERC721.sol
@@ -3,9 +3,8 @@ pragma solidity ^0.8.18;
 pragma abicoder v1;
 
 import {Access}                   from "Contracts/Util/Access.sol";
-import {Sign}                     from "Contracts/Util/Sign.sol";
+import {DID, Sign}                from "Contracts/Util/Sign.sol";
 import {DynamicPrice, Proxy}      from "Contracts/Util/DynamicPrice.sol";
-import {DID}                      from "Contracts/DID.sol";
 import {IERC721, IERC721Metadata} from "Contracts/Interfaces.sol";
 
 contract ERC721 is IERC721, IERC721Metadata, Access, Sign, DynamicPrice {
