62712354f5c8faac3b772abe604694fe5d5e97b1
aloycwl <aloycwl@gmail.com>
2023-06-30 14:26:34 +0800

    Updated pragma

--- Contracts/Interfaces.sol
+++ Contracts/Interfaces.sol
@@ -1,5 +1,6 @@
 //SPDX-License-Identifier:None
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.18;
+pragma abicoder v1;
 
 //被调用的接口
 
--- Contracts/Proxy.sol
+++ Contracts/Proxy.sol
@@ -1,5 +1,6 @@
 //SPDX-License-Identifier:None
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.18;
+pragma abicoder v1;
 
 import "Contracts/Util/Access.sol";
 
