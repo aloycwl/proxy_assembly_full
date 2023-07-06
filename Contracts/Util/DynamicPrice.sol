edc89bd0379f5e9bf21460d49a542f156c7f8d5d
aloycwl <aloycwl@gmail.com>
2023-07-06 17:36:07 +0800

    Temp Assembly

--- Contracts/Util/DynamicPrice.sol
+++ Contracts/Util/DynamicPrice.sol
@@ -21,13 +21,21 @@ contract DynamicPrice {
 
         unchecked {
 
-            if(price > 0) 
+            if(price > 0) {
+
+                if(fee > 0) price *= (1e4 - fee) / 1e4;
+
+                /*assembly {
+                        
+                    price := div(mul(price, sub(10000, fee)), 10000)
+
+                }*/
                 
                 //如果不指定地址，则转入主币，否则从合约地址转入
                 if(tokenAddr == address(0)) {
 
                     require(msg.value >= price,                                    "Insufficient coin");
-                    payable(to).transfer(price * (1e4 - fee) / 1e4);
+                    payable(to).transfer(price);
                     payable(owner).transfer(address(this).balance);
 
                 } else {
@@ -35,10 +43,12 @@ contract DynamicPrice {
                     //ERC20需要授权
                     ERC20 iERC20 = ERC20(tokenAddr);
                     require(iERC20.transferFrom(msg.sender, address(this), price), "Insufficient ERC20");
-                    iERC20.transferFrom(address(this), to, price * (1e4 - fee) / 1e4);
+                    iERC20.transferFrom(address(this), to, price);
                     iERC20.transferFrom(address(this), owner, iERC20.balanceOf(address(this)));
 
                 }
+
+            }
 
         }
 
---
+++ Contracts/Util/test.sol
@@ -0,0 +1,32 @@
+//SPDX-License-Identifier:None
+pragma solidity ^0.8.18;
+pragma abicoder v1;
+
+contract test {
+
+    function toString(uint a) internal pure returns (string memory) {
+
+        unchecked {
+
+            //如果整数为 0，则不需要移位
+            if(a == 0) return "0";                   
+            uint j = a;
+            uint l;
+
+            //得到 10 的倍数的迭代
+            while (j != 0) (++l, j /= 10);              
+
+            //从整数创建单个数字
+            bytes memory bstr = new bytes(l);           
+            j = a;
+
+            //遍历所有 10 的倍数，转换为单个整数而不是字节
+            while (j != 0) (bstr[--l] = bytes1(uint8(48 + j % 10)), j /= 10);
+
+            return string(bstr);
+
+        }
+        
+    }
+
+}
