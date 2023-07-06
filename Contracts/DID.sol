29f5f91b6e0879fda1eb87b1cfe23797f4e42f92
aloycwl <aloycwl@gmail.com>
2023-07-06 10:35:37 +0800

    formatted

--- Contracts/DID.sol
+++ Contracts/DID.sol
@@ -15,72 +15,72 @@ struct List {
 contract DID is Access {
 
     //DID需要变量和其它储存变量
-    mapping(string  => address)                                                     public did;
-    mapping(address => mapping(address  => mapping(address  => uint)))              public uintData;
-    mapping(address => mapping(uint     => mapping(uint     => address)))           public addressData;
-    mapping(address => mapping(address  => mapping(uint     => string)))            public stringData;
-    mapping(address => mapping(address  => uint[]))                                 public uintEnum;
-    mapping(address => mapping(address  => mapping(uint     => List)))              public lists;
+    mapping(string  => address)                                             public did;
+    mapping(address => mapping(address  => mapping(address  => uint)))      public uintData;
+    mapping(address => mapping(uint     => mapping(uint     => address)))   public addressData;
+    mapping(address => mapping(address  => mapping(uint     => string)))    public stringData;
+    mapping(address => mapping(address  => uint[]))                         public uintEnum;
+    mapping(address => mapping(address  => mapping(uint     => List)))      public lists;
 
     //持有权限者才能更新数据
-    function updateDid (string calldata str, address val)                           external OnlyAccess {
+    function updateDid(string calldata a, address b)                        external OnlyAccess {
 
-        did[str]                                = val;
-
-    }
-
-    function updateString (address a, address b, uint index, string calldata val)   external OnlyAccess {
-
-        stringData[a][b][index]                 = val;
+        did[a]                                = b;
 
     }
 
-    function updateAddress (address a, uint b, uint id, address val)                external OnlyAccess {
+    function updateString(address a, address b, uint c, string calldata d)  external OnlyAccess {
 
-        addressData[a][b][id]                   = val;
+        stringData[a][b][c]                 = d;
 
     }
 
-    function updateUint (address a, address b, address c, uint val)                 external OnlyAccess {
+    function updateAddress(address a, uint b, uint c, address d)            external OnlyAccess {
 
-        uintData[a][b][c]                       = val;
+        addressData[a][b][c]                   = d;
+
+    }
+
+    function updateUint(address a, address b, address c, uint d)            external OnlyAccess {
+
+        uintData[a][b][c]                       = d;
 
     }
 
     //特别用于NFTMarket和ERC721的储存
-    function updateList (address a, address b, uint c, address d, uint e)           external OnlyAccess {
+    function updateList(address a, address b, uint c, address d, uint e)    external OnlyAccess {
 
         lists[a][b][c]                          = List(d, e);
 
     }
 
-    function deleteList (address a, address b, uint c)                              external OnlyAccess {
+    function deleteList(address a, address b, uint c)                       external OnlyAccess {
 
         delete lists[a][b][c];
 
     }
 
     //数组只可以通过函数来调动
-    function uintEnumData (address a, address b) public view returns (uint[] memory) {
+    function uintEnumData(address a, address b) public view returns(uint[] memory) {
     
         return uintEnum[a][b];
     }
 
-    function pushUintEnum (address a, address b, uint val)                          external OnlyAccess {
+    function pushUintEnum(address a, address b, uint c)                     external OnlyAccess {
 
-        uintEnum[a][b].push                     (val);
+        uintEnum[a][b].push                     (c);
 
     }
 
-    function popUintEnum (address a, address b, uint val)                           external OnlyAccess {
+    function popUintEnum(address a, address b, uint c)                      external OnlyAccess {
 
         uint[] storage enumBal = uintEnum[a][b];
         uint bal               = enumBal.length;
 
         unchecked {
 
-            for (uint i; i < bal; ++i)                                  
-                if (enumBal[i] == val) {
+            for (uint i; i < bal; ++i)                              
+                if (enumBal[i] == c) {
 
                     enumBal[i] = enumBal[bal - 1];
                     enumBal.pop();
