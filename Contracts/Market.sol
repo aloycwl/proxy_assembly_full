194c95a49342b8e3ffcb4a3d76ee11470542e793
aloycwl <aloycwl@gmail.com>
2023-07-06 15:34:16 +0800

    Removed Proxy

--- Contracts/Market.sol
+++ Contracts/Market.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.18;
 pragma abicoder v1;
 
-import {DID, DynamicPrice, Proxy, IERC721, Access} from "Contracts/Util/DynamicPrice.sol";
+import {DID, DynamicPrice, IERC721, Access} from "Contracts/ERC721.sol";
 
 contract Market is Access, DynamicPrice {
 
-    Proxy private iProxy;
+    DID private iDID;
 
     event Item(address contAddr, address tokenAddr, uint tokenId, uint price);
 
-    constructor(address proxy) DynamicPrice (proxy) {
+    constructor(address did) DynamicPrice(did) {
 
-        iProxy = Proxy(proxy);
+        iDID = DID(did);
 
      }
 
@@ -23,10 +23,10 @@ contract Market is Access, DynamicPrice {
 
         IERC721 iERC721 = IERC721(contAddr);
 
-        require(iERC721.ownerOf(tokenId) == msg.sender,                             "Not owner");
-        require(iERC721.isApprovedForAll(msg.sender, address(this)),                "No approval");
+        require(iERC721.ownerOf(tokenId) == msg.sender,                 "Not owner");
+        require(iERC721.isApprovedForAll(msg.sender, address(this)),    "No approval");
 
-        DID(iProxy.addrs(3)).updateList(address(this), contAddr, tokenId, tokenAddr, price);
+        iDID.updateList(address(this), contAddr, tokenId, tokenAddr, price);
 
         emit Item(contAddr, tokenAddr, tokenId, price);
 
@@ -35,8 +35,8 @@ contract Market is Access, DynamicPrice {
     //取消列表功能，也将在成功购买时调用
     function delist(address contAddr, uint tokenId) public {
 
-        require(IERC721(contAddr).ownerOf(tokenId) == msg.sender,                   "Not owner");
-        DID(iProxy.addrs(3)).deleteList(address(this), contAddr, tokenId);
+        require(IERC721(contAddr).ownerOf(tokenId) == msg.sender,       "Not owner");
+        iDID.deleteList(address(this), contAddr, tokenId);
 
         emit Item(contAddr, address(0), tokenId, 0);
 
@@ -47,18 +47,18 @@ contract Market is Access, DynamicPrice {
 
         unchecked {
 
-            (, uint price) = DID(iProxy.addrs(3)).lists(address(this), contAddr, tokenId);
-            require(price > 0,                                                      "Item is not for sale");
+            (, uint price) = iDID.lists(address(this), contAddr, tokenId);
+            require(price > 0,                                          "Item is not for sale");
 
             IERC721 iERC721 = IERC721(contAddr);
             address seller  = iERC721.ownerOf(tokenId);
 
-            iERC721.approve(msg.sender, tokenId);                                   //手动授权给新所有者
+            iERC721.approve(msg.sender, tokenId);                       //手动授权给新所有者
             iERC721.transferFrom(seller, msg.sender, tokenId);
 
-            pay(contAddr, tokenId, seller, fee);                                    //转币给卖家减费用
+            pay(contAddr, tokenId, seller, fee);                        //转币给卖家减费用
             
-            delist(contAddr, tokenId);                                              //把币下市
+            delist(contAddr, tokenId);                                  //把币下市
 
         }
 
