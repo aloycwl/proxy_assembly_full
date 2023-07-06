6f8ed329b45a79ebdbc2da3b6475466b067ca0ff
aloycwl <aloycwl@gmail.com>
2023-07-06 18:22:01 +0800

    Assembly in progress

--- Contracts/Util/test.sol
+++ Contracts/Util/test.sol
@@ -4,24 +4,30 @@ pragma abicoder v1;
 
 contract test {
 
-    function toString(uint a) internal pure returns (string memory) {
+    function toString(uint a) external pure returns (string memory) {
 
         unchecked {
 
             //如果整数为 0，则不需要移位
             if(a == 0) return "0";                   
-            uint j = a;
+            uint j;
             uint l;
 
             //得到 10 的倍数的迭代
-            while (j != 0) (++l, j /= 10);              
+            //while (j != 0) (++l, j /= 10);
+            for(j = a; j > 0; j /= 10) ++l;
+                
+            /*assembly {
+                for { j := a } sgt(j, 0) { j := div(j, 10) } {
+                    l := add(l, 1)
+                }
+            }*/
 
             //从整数创建单个数字
-            bytes memory bstr = new bytes(l);           
-            j = a;
+            bytes memory bstr = new bytes(l);
 
             //遍历所有 10 的倍数，转换为单个整数而不是字节
-            while (j != 0) (bstr[--l] = bytes1(uint8(48 + j % 10)), j /= 10);
+            for(j = a; j > 0; j /= 10) bstr[--l] = bytes1(uint8(48 + j % 10));
 
             return string(bstr);
 
