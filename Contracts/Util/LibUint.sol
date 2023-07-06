311391d2b5834db4ffc83d101edd85efde3d2bbb
aloycwl <aloycwl@gmail.com>
2023-07-06 20:08:36 +0800

    assembly testing

--- Contracts/Util/LibUint.sol
+++ Contracts/Util/LibUint.sol
@@ -9,20 +9,14 @@ library LibUint {
 
         unchecked {
 
-            //如果整数为 0，则不需要移位
             if(a == 0) return "0";                   
-            uint j = a;
             uint l;
 
-            //得到 10 的倍数的迭代
-            while (j != 0) (++l, j /= 10);              
+            for(uint j = a; j > 0; j /= 10) ++l;
 
-            //从整数创建单个数字
-            bytes memory bstr = new bytes(l);           
-            j = a;
+            bytes memory bstr = new bytes(l);
 
-            //遍历所有 10 的倍数，转换为单个整数而不是字节
-            while (j != 0) (bstr[--l] = bytes1(uint8(48 + j % 10)), j /= 10);
+            for(uint j = a; j > 0; j /= 10) bstr[--l] = bytes1(uint8(j % 10 + 48));
 
             return string(bstr);
 
--- Contracts/Util/test.sol
+++ Contracts/Util/test.sol
@@ -8,26 +8,20 @@ contract test {
 
         unchecked {
 
-            //如果整数为 0，则不需要移位
             if(a == 0) return "0";                   
-            uint j;
             uint l;
 
-            //得到 10 的倍数的迭代
-            //while (j != 0) (++l, j /= 10);
-            for(j = a; j > 0; j /= 10) ++l;
+            for(uint j = a; j > 0; j /= 10) ++l;
                 
             /*assembly {
-                for { j := a } sgt(j, 0) { j := div(j, 10) } {
+                for { let j := a } sgt(j, 0) { j := div(j, 10) } {
                     l := add(l, 1)
                 }
             }*/
 
-            //从整数创建单个数字
             bytes memory bstr = new bytes(l);
 
-            //遍历所有 10 的倍数，转换为单个整数而不是字节
-            for(j = a; j > 0; j /= 10) bstr[--l] = bytes1(uint8(48 + j % 10));
+            for(uint j = a; j > 0; j /= 10) bstr[--l] = bytes1(uint8(j % 10 + 48));
 
             return string(bstr);
 
