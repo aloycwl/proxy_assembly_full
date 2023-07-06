98e4118dfaa902ceb92565871abe68ed4b02ca71
aloycwl <aloycwl@gmail.com>
2023-07-06 15:12:16 +0800

    Removed proxy

--- Contracts/Util/Sign.sol
+++ Contracts/Util/Sign.sol
@@ -3,24 +3,21 @@ pragma solidity ^0.8.18;
 pragma abicoder v1;
 
 import {LibUint} from "Contracts/Util/LibUint.sol";
-import {Proxy}   from "Contracts/Proxy.sol";
 import {DID}     from "Contracts/DID.sol";
 
 contract Sign {
 
     using LibUint for uint;
+    
+    DID public iDID;
 
-    Proxy private iProxy;
+    constructor(address did) {
 
-    constructor(address proxy) {
-
-        iProxy = Proxy(proxy);
+        iDID = DID(did);
 
     }
 
     function check(address addr, uint8 v, bytes32 r, bytes32 s) internal {
-
-        DID iDID = DID(iProxy.addrs(3));     //签名者用3号索引
         
         require(          
             ecrecover(
@@ -30,7 +27,7 @@ contract Sign {
                         iDID.uintData(address(this), addr, address(1)).toString())
                     )
                 )
-            )), v, r, s) == iProxy.addrs(4), "Invalid signature");
+            )), v, r, s) == iDID.addressData(address(0), 0, 0), "Invalid signature");
             
         //更新计数器以防止类似的散列，并更新最后的时间戳
         iDID.updateUint(address(this), addr, address(1), block.timestamp);
