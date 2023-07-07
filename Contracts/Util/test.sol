0ed95b3dd21609dbe48a1108676a1fb281a50bec
aloycwl <aloycwl@gmail.com>
2023-07-07 14:40:54 +0800

    Testing assembly

--- Contracts/Util/test.sol
+++ Contracts/Util/test.sol
@@ -20,11 +20,35 @@ contract test {
                 }
             }*/
 
-            bytes memory bstr = new bytes(l);
+            /*bytes memory bstr = new bytes(l);
 
             for(uint j = a; j > 0x00; j /= 0x0A) bstr[--l] = bytes1(uint8(j % 0x0A + 0x30));
 
-            return string(bstr);
+            return string(bstr);*/
+
+            assembly {
+                // Set the memory location for the bytes array
+                let bstr := mload(0x40)
+                // Store the length of the bytes array
+                mstore(bstr, l)
+
+                for {let j := a} gt(j, 0x00) { j := div(j, 0x0A) } {
+                    // Calculate j % 0x0A
+                    let remainder := mod(j, 0x0A)
+
+                    // Calculate the index for the bytes array
+                    let index := sub(l, 1)
+
+                    // Store the ASCII representation of the remainder in the bytes array
+                    mstore8(add(bstr, index), add(remainder, 0x30))
+
+                    // Decrement the loop counter l
+                    l := sub(l, 1)
+                }
+
+                // Set the bytes array as the return value
+                return(bstr, l)
+            }
 
         }
         
