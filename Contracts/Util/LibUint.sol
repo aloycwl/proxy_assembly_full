b70852a4d83181ec49ea4dd571cc601ce2d4b7a9
aloycwl <aloycwl@gmail.com>
2023-07-10 18:45:18 +0800

    Assembly

--- Contracts/Util/LibUint.sol
+++ Contracts/Util/LibUint.sol
@@ -5,17 +5,22 @@ pragma abicoder v1;
 library LibUint {
 
     //整数转移字符
-    function toString(uint a) internal pure returns (string memory) {
+    function toString(uint a) internal pure returns (string memory val) {
 
         assembly {
+            
                 
             let l
 
-            mstore(0x20, 0x20)
+            //mstore(0x20, 0x20)
+            //mstore(0x40, 0x20)
+            val := mload(0x40)
             mstore(0x40, 0x20)
+            mstore(val, 0x20)
 
             if iszero(a) {
-                mstore8(0x60, 0x30)
+                //mstore8(0x60, 0x30)
+                mstore(add(val, 0x20), 0x30)
             }
 
             for { let j := a } gt(j, 0x0) { j := div(j, 0xa) } {
@@ -24,10 +29,11 @@ library LibUint {
 
             for { } gt(a, 0x0) { a := div(a, 0xa) } {
                 l := sub(l, 0x1)
-                mstore8(add(l, 0x60), add(mod(a, 0xa), 0x30))
+                //mstore8(add(l, 0x60), add(mod(a, 0xa), 0x30))
+                mstore8(add(l, add(val, 0x20)), add(mod(a, 0xa), 0x30))
             }
 
-            return(0x20, 0x60)
+            //return(0x20, 0x60)
 
         }
         
