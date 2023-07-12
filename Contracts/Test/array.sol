fff5c67b785ba9c6070e3b80ccfa019f0136aa2a
aloycwl <aloycwl@gmail.com>
2023-07-12 19:15:03 +0800

    asm

--- Contracts/Test/array.sol
+++ Contracts/Test/array.sol
@@ -4,11 +4,11 @@ pragma abicoder v1;
 
 contract arraywy {
 
-    function secondTest() public pure returns (uint32[] memory res) {                                                                    
+    function secondTest() public pure returns (uint[] memory res) {                                                                    
         assembly {                                                                                      
             res := mload(0x40)
-            mstore(0x40, add(add(res, 0x20), 640))
-            mstore(res, 20) 
+            mstore(0x40, add(add(res, 0x20), 0x140))
+            mstore(res, 10) 
         }                                                                         
     }    
 }
