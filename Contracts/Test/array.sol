edca57f1be2394e00dcdb1dc8b5e14255f0559e4
aloycwl <aloycwl@gmail.com>
2023-07-14 22:54:44 +0800

    array assembly

--- Contracts/Test/array.sol
+++ Contracts/Test/array.sol
@@ -20,64 +20,47 @@ contract arraywy {
 
     function pushToArray(uint256 element) external {
         assembly {
-            // Load the length of the array
             let length := sload(myArray.slot)
 
-            // Resize the array by increasing the length
             sstore(myArray.slot, add(length, 1))
 
-            // Calculate the position to store the new element
             let position := add(myArray.slot, mul(length, 32))
 
-            // Store the element at the calculated position
             sstore(position, element)
 
             sstore(myArray.slot, add(length, 1))
         }
-    }
-
-    function getValueAtIndex(uint256 index) external view returns (uint value) {
-        assembly {
-            // Load the length of the array
-            let length := sload(myArray.slot)
-
-            // Check if the index is within bounds
-            if iszero(lt(index, length)) {
-                revert(0, 0)
-            }
-
-            // Calculate the position of the desired element
-            let position := add(myArray.slot, add(mul(add(index, 1), 32), 32))
-
-            // Load the value at the calculated position
-            value := sload(position)
-        }
     }*/
 
-    uint256[] public myArray;
+    //uint256[] public myArray;
 
     function pushar(uint val) external {
-        myArray.push(val);
+        assembly {
+            let ptr := 0x0
+            let len := sload(ptr)
+            sstore(ptr, add(len, 1))
+            mstore(0x0, ptr)
+            sstore(add(keccak256(0x0, 0x20), mul(len, 0x1)), val)
+        }
     }
 
-    function getArray() external view returns (uint256[] memory result) {
-        uint256 length = myArray.length;
+    function getArray() external view returns (uint[] memory result) {
 
-        result = new uint256[](length);
+        uint len;
+        
+        assembly {
+            let ptr := 0x0
+            mstore(0x0, ptr)
+            len := sload(ptr)
+        }
+
+        result = new uint[](len);
 
         assembly {
-            // Load the pointer to myArray data
-            let dataPointer := sload(myArray.slot)
-            mstore(0x0, dataPointer)
-
-            // Set the result length
-            mstore(result, length)
-
-            // Copy each element of myArray to result
-            for { let i := 0 } lt(i, length) { i := add(i, 1) } {
-                let elementPointer := add(dataPointer, mul(i, 0x20))
-                let element := sload(elementPointer)
-                mstore(add(result, add(0x20, mul(i, 0x20))), element)
+            mstore(result, len)
+            let ptr := keccak256(0x0, 0x20)
+            for { let i := 0x0 } lt(i, len) { i := add(i, 0x1) } {
+                mstore(add(result, mul(add(i, 0x1), 0x20)), sload(add(ptr, i)))
             }
         }
     }
