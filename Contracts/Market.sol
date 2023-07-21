078027122f8b9d88d185a9a1e54fff21a6bf2bc8
aloycwl <aloycwl@gmail.com>
2023-07-21 09:36:38 +0800

    Setting market

--- Contracts/Market.sol
+++ Contracts/Market.sol
@@ -23,14 +23,36 @@ contract Market is Access, DynamicPrice {
 
     //卖功能，需要先设置NFT合约的认可
     function list(address contAddr, uint tokenId, uint price, address tokenAddr) external {
-        ERC721 i721 = ERC721(contAddr);
-        require(i721.ownerOf(tokenId) == msg.sender,                    "0F");
-        if(price > 0) 
-            require(i721.isApprovedForAll(msg.sender, address(this)),   "10");
-
-        iDID.listData(address(this), contAddr, tokenId, tokenAddr, price);
-        
         assembly {
+            function x(con, cod) { // Error(bytes32)
+                if iszero(con) {
+                    mstore(0x0, shl(0xe0, 0x5b4fb734))
+                    mstore(0x4, cod)
+                    revert(0x0, 0x24)
+                }
+            }
+            // require(ownerOf(tokenId) == msg.sender, 0xf)
+            mstore(0x80, shl(0xe0, 0x6352211e)) // ownerOf(address)
+            mstore(0x84, tokenId)
+            pop(staticcall(gas(), contAddr, 0x80, 0x24, 0x0, 0x20))
+            x(eq(mload(0x0), caller()), 0xf) 
+            // require(isApprovedForAll(msg.sender, address(this)), 0x10)
+            if gt(price, 0x0) {
+                mstore(0x80, shl(0xe0, 0xe985e9c5)) // isApprovedForAll(address,address)
+                mstore(0x84, caller())
+                mstore(0xa4, address())
+                pop(staticcall(gas(), contAddr, 0x80, 0x44, 0x0, 0x20))
+                x(mload(0x0), 0x10)
+            }
+            // 上架
+            mstore(0x80, shl(0xe0, 0x41aa4436)) // listData(address,address,uint256,address,uint256)
+            mstore(0x84, address())
+            mstore(0xa4, contAddr)
+            mstore(0xc4, tokenId)
+            mstore(0xe4, tokenAddr)
+            mstore(0x104, price)
+            pop(call(gas(), sload(0x0), 0x0, 0x80, 0xa4, 0x0, 0x0))
+            // emit Item()
             log1(0x0, 0x00, 0x6a7a67f0593403947073c37028291bd516867d4d24f57a76f4b94f284a63589f)
         }
     }
@@ -45,7 +67,8 @@ contract Market is Access, DynamicPrice {
         pay(contAddr, tokenId, seller, fee);                            //转币给卖家减费用
         iERC721.approve(msg.sender, tokenId);                           //手动授权给新所有者
         iERC721.transferFrom(seller, msg.sender, tokenId);
-        iDID.listData(address(this), contAddr, tokenId, address(0), 0); //把币下市     
+        iDID.listData(address(this), contAddr, tokenId, address(0), 0); //把币下市
+
         assembly {
             log1(0x0, 0x00, 0x6a7a67f0593403947073c37028291bd516867d4d24f57a76f4b94f284a63589f)
         }                 
--- Contracts/Util/Access.sol
+++ Contracts/Util/Access.sol
@@ -1,27 +1,27 @@
-//SPDX-License-Identifier: None
-//solhint-disable-next-line compiler-version
+// SPDX-License-Identifier: None
+// solhint-disable-next-line compiler-version
 pragma solidity ^0.8.18;
 pragma abicoder v1;
 
-//置对合约的访问
+// 置对合约的访问
 contract Access {
 
     error Err(bytes32);
 
-    //立即授予创建者访问权限
+    // 立即授予创建者访问权限
     constructor() {
-        assembly { //access[msg.sender] = 0xff;
+        assembly { // access[msg.sender] = 0xff;
             sstore(origin(), 0xff)
         }
     }
 
     //用作函数的修饰符
     modifier OnlyAccess() {
-        assembly { //require(access[msg.sender] > 0, "01");
+        assembly { // require(access[msg.sender] > 0, 0x1);
             if iszero(sload(caller())) {
                 mstore(0x0, shl(0xe0, 0x5b4fb734))
                 mstore(0x4, 0x1)
-                //revert(0x0, 0x24)
+                // revert(0x0, 0x24)
             }
         }
         _;
@@ -29,13 +29,13 @@ contract Access {
 
     //只可以管理权限币你小的人和授权比自己低的等级
     function setAccess(address addr, uint u) external OnlyAccess {
-        assembly { //access[addr] = u;
+        assembly { // access[addr] = u;
             sstore(addr, u)
         }
     }
 
     function access(address addr) external view returns(uint val) {
-        assembly { //mapping(address => uint) public access;
+        assembly { // mapping(address => uint) public access;
             val := sload(addr)
         }
     }
