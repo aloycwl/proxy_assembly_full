ef0f0422b679872d0374b40e4d3d36efc23331b6
aloycwl <aloycwl@gmail.com>
2023-07-24 15:58:50 +0800

    Doing proxy

--- .deps/npm/@openzeppelin/contracts-upgradeable/proxy/ERC1967/ERC1967UpgradeUpgradeable.sol
+++ .deps/npm/@openzeppelin/contracts-upgradeable/proxy/ERC1967/ERC1967UpgradeUpgradeable.sol
@@ -3,83 +3,86 @@
 
 pragma solidity ^0.8.2;
 
-import "../beacon/IBeaconUpgradeable.sol";
 import "../../interfaces/IERC1967Upgradeable.sol";
 import "../../interfaces/draft-IERC1822Upgradeable.sol";
-import "../../utils/StorageSlotUpgradeable.sol";
 import "../utils/Initializable.sol";
+
+library StorageSlotUpgradeable {
+    struct AddressSlot { address value; }
+    struct BooleanSlot { bool value; }
+    struct Bytes32Slot { bytes32 value; }
+    struct Uint256Slot { uint256 value; }
+    struct StringSlot { string value; }
+    struct BytesSlot { bytes value; }
+    function getAddressSlot(bytes32 slot) internal pure returns (AddressSlot storage r) {
+        assembly {
+            r.slot := slot
+        }
+    }
+    function getBooleanSlot(bytes32 slot) internal pure returns (BooleanSlot storage r) {
+        assembly {
+            r.slot := slot
+        }
+    }
+    function getBytes32Slot(bytes32 slot) internal pure returns (Bytes32Slot storage r) {
+        assembly {
+            r.slot := slot
+        }
+    }
+    function getUint256Slot(bytes32 slot) internal pure returns (Uint256Slot storage r) {
+        assembly {
+            r.slot := slot
+        }
+    }
+    function getStringSlot(bytes32 slot) internal pure returns (StringSlot storage r) {
+        assembly {
+            r.slot := slot
+        }
+    }
+    function getStringSlot(string storage store) internal pure returns (StringSlot storage r) {
+        assembly {
+            r.slot := store.slot
+        }
+    }
+    function getBytesSlot(bytes32 slot) internal pure returns (BytesSlot storage r) {
+        assembly {
+            r.slot := slot
+        }
+    }
+    function getBytesSlot(bytes storage store) internal pure returns (BytesSlot storage r) {
+        assembly {
+            r.slot := store.slot
+        }
+    }
+}
  
-/**
- * @dev This abstract contract provides getters and event emitting update functions for
- * https://eips.ethereum.org/EIPS/eip-1967[EIP1967] slots.
- *
- * _Available since v4.1._
- */
+interface IBeaconUpgradeable {
+    function implementation() external view returns (address);
+}
+
 abstract contract ERC1967UpgradeUpgradeable is Initializable, IERC1967Upgradeable {
-    function __ERC1967Upgrade_init() internal onlyInitializing {
-    }
-
-    function __ERC1967Upgrade_init_unchained() internal onlyInitializing {
-    }
-    // This is the keccak-256 hash of "eip1967.proxy.rollback" subtracted by 1
+    function __ERC1967Upgrade_init() internal onlyInitializing {}
+    function __ERC1967Upgrade_init_unchained() internal onlyInitializing {}
     bytes32 private constant _ROLLBACK_SLOT = 0x4910fdfa16fed3260ed0e7147f7cc6da11a60208b5b9406d12a635614ffd9143;
-
-    /**
-     * @dev Storage slot with the address of the current implementation.
-     * This is the keccak-256 hash of "eip1967.proxy.implementation" subtracted by 1, and is
-     * validated in the constructor.
-     */
     bytes32 internal constant _IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
-
-    /**
-     * @dev Returns the current implementation address.
-     */
     function _getImplementation() internal view returns (address) {
         return StorageSlotUpgradeable.getAddressSlot(_IMPLEMENTATION_SLOT).value;
     }
-
-    /**
-     * @dev Stores a new address in the EIP1967 implementation slot.
-     */
     function _setImplementation(address newImplementation) private {
         require(AddressUpgradeable.isContract(newImplementation), "ERC1967: new implementation is not a contract");
         StorageSlotUpgradeable.getAddressSlot(_IMPLEMENTATION_SLOT).value = newImplementation;
     }
-
-    /**
-     * @dev Perform implementation upgrade
-     *
-     * Emits an {Upgraded} event.
-     */
     function _upgradeTo(address newImplementation) internal {
         _setImplementation(newImplementation);
         emit Upgraded(newImplementation);
     }
-
-    /**
-     * @dev Perform implementation upgrade with additional setup call.
-     *
-     * Emits an {Upgraded} event.
-     */
     function _upgradeToAndCall(address newImplementation, bytes memory data, bool forceCall) internal {
         _upgradeTo(newImplementation);
-        if (data.length > 0 || forceCall) {
-            AddressUpgradeable.functionDelegateCall(newImplementation, data);
-        }
+        if (data.length > 0 || forceCall) AddressUpgradeable.functionDelegateCall(newImplementation, data);
     }
-
-    /**
-     * @dev Perform implementation upgrade with security checks for UUPS proxies, and additional setup call.
-     *
-     * Emits an {Upgraded} event.
-     */
     function _upgradeToAndCallUUPS(address newImplementation, bytes memory data, bool forceCall) internal {
-        // Upgrades from old implementations will perform a rollback test. This test requires the new
-        // implementation to upgrade back to the old, non-ERC1822 compliant, implementation. Removing
-        // this special case will break upgrade paths from old UUPS implementation to new ones.
-        if (StorageSlotUpgradeable.getBooleanSlot(_ROLLBACK_SLOT).value) {
-            _setImplementation(newImplementation);
-        } else {
+        if (StorageSlotUpgradeable.getBooleanSlot(_ROLLBACK_SLOT).value) _setImplementation(newImplementation);
+        else {
             try IERC1822ProxiableUpgradeable(newImplementation).proxiableUUID() returns (bytes32 slot) {
                 require(slot == _IMPLEMENTATION_SLOT, "ERC1967Upgrade: unsupported proxiableUUID");
             } catch {
@@ -88,82 +91,34 @@ abstract contract ERC1967UpgradeUpgradeable is Initializable, IERC1967Upgradeabl
             _upgradeToAndCall(newImplementation, data, forceCall);
         }
     }
-
-    /**
-     * @dev Storage slot with the admin of the contract.
-     * This is the keccak-256 hash of "eip1967.proxy.admin" subtracted by 1, and is
-     * validated in the constructor.
-     */
     bytes32 internal constant _ADMIN_SLOT = 0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103;
-
-    /**
-     * @dev Returns the current admin.
-     */
     function _getAdmin() internal view returns (address) {
         return StorageSlotUpgradeable.getAddressSlot(_ADMIN_SLOT).value;
     }
-
-    /**
-     * @dev Stores a new address in the EIP1967 admin slot.
-     */
     function _setAdmin(address newAdmin) private {
         require(newAdmin != address(0), "ERC1967: new admin is the zero address");
         StorageSlotUpgradeable.getAddressSlot(_ADMIN_SLOT).value = newAdmin;
     }
-
-    /**
-     * @dev Changes the admin of the proxy.
-     *
-     * Emits an {AdminChanged} event.
-     */
     function _changeAdmin(address newAdmin) internal {
         emit AdminChanged(_getAdmin(), newAdmin);
         _setAdmin(newAdmin);
     }
-
-    /**
-     * @dev The storage slot of the UpgradeableBeacon contract which defines the implementation for this proxy.
-     * This is bytes32(uint256(keccak256('eip1967.proxy.beacon')) - 1)) and is validated in the constructor.
-     */
     bytes32 internal constant _BEACON_SLOT = 0xa3f0ad74e5423aebfd80d3ef4346578335a9a72aeaee59ff6cb3582b35133d50;
-
-    /**
-     * @dev Returns the current beacon.
-     */
     function _getBeacon() internal view returns (address) {
         return StorageSlotUpgradeable.getAddressSlot(_BEACON_SLOT).value;
     }
-
-    /**
-     * @dev Stores a new beacon in the EIP1967 beacon slot.
-     */
     function _setBeacon(address newBeacon) private {
         require(AddressUpgradeable.isContract(newBeacon), "ERC1967: new beacon is not a contract");
-        require(
-            AddressUpgradeable.isContract(IBeaconUpgradeable(newBeacon).implementation()),
+        require(AddressUpgradeable.isContract(IBeaconUpgradeable(newBeacon).implementation()),
             "ERC1967: beacon implementation is not a contract"
         );
         StorageSlotUpgradeable.getAddressSlot(_BEACON_SLOT).value = newBeacon;
     }
-
-    /**
-     * @dev Perform beacon upgrade with additional setup call. Note: This upgrades the address of the beacon, it does
-     * not upgrade the implementation contained in the beacon (see {UpgradeableBeacon-_setImplementation} for that).
-     *
-     * Emits a {BeaconUpgraded} event.
-     */
     function _upgradeBeaconToAndCall(address newBeacon, bytes memory data, bool forceCall) internal {
         _setBeacon(newBeacon);
         emit BeaconUpgraded(newBeacon);
-        if (data.length > 0 || forceCall) {
-            AddressUpgradeable.functionDelegateCall(IBeaconUpgradeable(newBeacon).implementation(), data);
-        }
+        if (data.length > 0 || forceCall) AddressUpgradeable.functionDelegateCall(IBeaconUpgradeable(newBeacon).implementation(), data);
+        
     }
-
-    /**
-     * @dev This empty reserved space is put in place to allow future versions to add new
-     * variables without shifting down storage in the inheritance chain.
-     * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
-     */
     uint256[50] private __gap;
 }
--- .deps/npm/@openzeppelin/contracts-upgradeable/utils/StorageSlotUpgradeable.sol
+++ .deps/npm/@openzeppelin/contracts-upgradeable/utils/StorageSlotUpgradeable.sol
@@ -31,106 +31,49 @@ pragma solidity ^0.8.0;
  * _Available since v4.1 for `address`, `bool`, `bytes32`, `uint256`._
  * _Available since v4.9 for `string`, `bytes`._
  */
-library StorageSlotUpgradeable {
-    struct AddressSlot {
-        address value;
-    }
-
-    struct BooleanSlot {
-        bool value;
-    }
-
-    struct Bytes32Slot {
-        bytes32 value;
-    }
-
-    struct Uint256Slot {
-        uint256 value;
-    }
-
-    struct StringSlot {
-        string value;
-    }
-
-    struct BytesSlot {
-        bytes value;
-    }
-
-    /**
-     * @dev Returns an `AddressSlot` with member `value` located at `slot`.
-     */
+library StorageSlotUpgradeablse {
+    struct AddressSlot { address value; }
+    struct BooleanSlot { bool value; }
+    struct Bytes32Slot { bytes32 value; }
+    struct Uint256Slot { uint256 value; }
+    struct StringSlot { string value; }
+    struct BytesSlot { bytes value; }
     function getAddressSlot(bytes32 slot) internal pure returns (AddressSlot storage r) {
-        /// @solidity memory-safe-assembly
         assembly {
             r.slot := slot
         }
     }
-
-    /**
-     * @dev Returns an `BooleanSlot` with member `value` located at `slot`.
-     */
     function getBooleanSlot(bytes32 slot) internal pure returns (BooleanSlot storage r) {
-        /// @solidity memory-safe-assembly
         assembly {
             r.slot := slot
         }
     }
-
-    /**
-     * @dev Returns an `Bytes32Slot` with member `value` located at `slot`.
-     */
     function getBytes32Slot(bytes32 slot) internal pure returns (Bytes32Slot storage r) {
-        /// @solidity memory-safe-assembly
         assembly {
             r.slot := slot
         }
     }
-
-    /**
-     * @dev Returns an `Uint256Slot` with member `value` located at `slot`.
-     */
     function getUint256Slot(bytes32 slot) internal pure returns (Uint256Slot storage r) {
-        /// @solidity memory-safe-assembly
         assembly {
             r.slot := slot
         }
     }
-
-    /**
-     * @dev Returns an `StringSlot` with member `value` located at `slot`.
-     */
     function getStringSlot(bytes32 slot) internal pure returns (StringSlot storage r) {
-        /// @solidity memory-safe-assembly
         assembly {
             r.slot := slot
         }
     }
-
-    /**
-     * @dev Returns an `StringSlot` representation of the string storage pointer `store`.
-     */
     function getStringSlot(string storage store) internal pure returns (StringSlot storage r) {
-        /// @solidity memory-safe-assembly
         assembly {
             r.slot := store.slot
         }
     }
-
-    /**
-     * @dev Returns an `BytesSlot` with member `value` located at `slot`.
-     */
     function getBytesSlot(bytes32 slot) internal pure returns (BytesSlot storage r) {
-        /// @solidity memory-safe-assembly
         assembly {
             r.slot := slot
         }
     }
-
-    /**
-     * @dev Returns an `BytesSlot` representation of the bytes storage pointer `store`.
-     */
     function getBytesSlot(bytes storage store) internal pure returns (BytesSlot storage r) {
-        /// @solidity memory-safe-assembly
         assembly {
             r.slot := store.slot
         }
