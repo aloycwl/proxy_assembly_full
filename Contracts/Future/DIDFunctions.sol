fb1f8f2c79978a6e9e892023a1a6ae5db58c5829
aloycwl <aloycwl@gmail.com>
2023-07-27 09:42:59 +0800

    Updated DIDFunctions to Assembly

--- Contracts/Future/DIDFunctions.sol
+++ Contracts/Future/DIDFunctions.sol
@@ -1,50 +1,92 @@
-//SPDX-License-Identifier:None
+// SPDX-License-Identifier: None
+// solhint-disable-next-line compiler-version
 pragma solidity ^0.8.18;
 pragma abicoder v1;
 
-import {Access, DID} from "Contracts/DID.sol";
+contract DIDFunctions {
 
-contract DIDFunctions is Access {
+    // 用户名不能重复
+    modifier OnlyUnique(string memory usn) {
+        assembly {
+            // address addr = stringData(address(), id)
+            mstore(0x80, 0x99eec06400000000000000000000000000000000000000000000000000000000)
+            mstore(0x84, caller())
+            mstore(0xa4, 0x0)
+            pop(staticcall(gas(), sload(0x0), 0x80, 0x44, 0x0, 0x20))
 
-    DID iDID;
-
-    //用户名不能重复
-    modifier OnlyUnique(string calldata userName) {
-
-        require(iDID.did(userName) == address(0), "Username existed");
+            // require(addr = address(0))
+            if gt(mload(0x0), 0x0) {
+                mstore(0xA4, 0x10)
+                mstore(0xC4, "Username existed")
+                revert(0x80, 0x64)
+            }
+        }
         _;
-
     }
 
     constructor(address did) {
-
-        //调用主合约
-        iDID = DID(did);
-
+        // iDID = DID(did);
+        assembly {
+            sstore(0x0, did)
+        }
     }
 
-    //谁都可以创造新用户
-    function createUser(address addr, string calldata userName, string calldata name, string calldata bio) 
-        external OnlyUnique(userName) {
-
-            iDID.did(userName, addr);             //创建用户名
-            iDID.stringData(addr, 0, userName);       //能够从地址中找到用户名
-            iDID.stringData(addr, 1, name);           //添加名称
-            iDID.stringData(addr, 2, bio);            //添加传记
-
+    // 创造新用户
+    function create(string memory usn, string memory nam, string memory bio) external OnlyUnique(usn) {
+        assembly {
+            // did[msg.sender] = usn 
+            mstore(0x80, 0x7148bc7200000000000000000000000000000000000000000000000000000000)
+            mstore(0x84, mload(add(0x20, usn)))
+            mstore(0xa4, caller())
+            pop(call(gas(), sload(0x0), 0x0, 0x80, 0x44, 0x0, 0x0))
+        }
+        update(0x0, usn);
+        update(0x1, nam);
+        update(0x1, bio);
     }
 
-    //改用户名，删除旧名来省燃料
-    function changeUsername(string calldata before, string calldata aft) external OnlyUnique(aft) {
+    // 改用户名
+    function update(string memory aft) external OnlyUnique(aft) {
+        assembly {
+            // address bef = stringData(address(), id)
+            mstore(0x80, 0x99eec06400000000000000000000000000000000000000000000000000000000)
+            mstore(0x84, caller())
+            mstore(0xa4, 0x0)
+            pop(staticcall(gas(), sload(0x0), 0x80, 0x44, 0x0, 0x20))
 
-        address addr = iDID.did(before);
+            // did[msg.sender] = address(0)
+            mstore(0x80, 0x7148bc7200000000000000000000000000000000000000000000000000000000)
+            mstore(0x84, mload(add(0x20, mload(0x0))))
+            mstore(0xa4, 0x0)
+            pop(call(gas(), sload(0x0), 0x0, 0x80, 0x44, 0x0, 0x0))
 
-        assert(msg.sender == addr);                     //只有所有者可以更改他们的用户名
-        
-        iDID.did(before, address(0));             //删除旧用户名
-        iDID.did(aft, addr);                      //添加新用户名
-        iDID.stringData(addr, 0, aft);                //更新地址搜索
+            // did[msg.sender] = aft 
+            mstore(0x84, mload(add(0x20, aft)))
+            mstore(0xa4, caller())
+            pop(call(gas(), sload(0x0), 0x0, 0x80, 0x44, 0x0, 0x0))
 
+            // stringData[msg.sender][0] = usn
+            mstore(0x80, 0xc7070b5800000000000000000000000000000000000000000000000000000000)
+            mstore(0x84, caller())
+            mstore(0xa4, 0x0)
+            mstore(0xc4, mload(aft))
+            mstore(0xe4, mload(add(aft, 0x20)))
+            mstore(0x104, mload(add(aft, 0x40)))
+            pop(call(gas(), sload(0x0), 0x0, 0x80, 0xa4, 0x0, 0x0))
+        }
     }
-    
+
+    // 更新其它资料
+    function update(uint typ, string memory inf) public {
+        assembly {
+            // stringData[msg.sender][typ] = inf
+            mstore(0x80, 0xc7070b5800000000000000000000000000000000000000000000000000000000)
+            mstore(0x84, caller())
+            mstore(0xa4, typ)
+            mstore(0xc4, mload(inf))
+            mstore(0xe4, mload(add(inf, 0x20)))
+            mstore(0x104, mload(add(inf, 0x40)))
+            pop(call(gas(), sload(0x0), 0x0, 0x80, 0xa4, 0x0, 0x0))
+        }
+    }
 }
