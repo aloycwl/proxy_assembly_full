40d466a39152545d652872051378cd8d33740827
aloycwl <aloycwl@gmail.com>
2023-07-28 16:52:13 +0800

    Updated call function

--- Contracts/Future/Test.sol
+++ Contracts/Future/Test.sol
@@ -2,11 +2,23 @@
 pragma solidity ^0.8.18;
 
 contract TEST {
-    function getSelector(string memory s) external pure returns(bytes4) {
-        return bytes4(keccak256(abi.encodePacked(s)));
+    function getSelector(string memory a) external pure returns(bytes4) {
+        return bytes4(keccak256(abi.encodePacked(a)));
     }
 
-    function getKeccak256(string memory s) external pure returns(bytes32) {
-        return keccak256(abi.encodePacked(s));
+    function getKeccak256(string memory a) external pure returns(bytes32) {
+        return keccak256(abi.encodePacked(a));
+    }
+
+    function convertAddr2Uint(address a) external pure returns(uint val) {
+        assembly {
+            val := a
+        }
+    }
+
+    function convertUint2Addr(uint a) external pure returns(address val) {
+        assembly {
+            val := a
+        }
     }
 }
