fe4f3c0b60c7fd713a5521130a75b73c4ed9bce7
aloycwl <aloycwl@gmail.com>
2023-08-10 13:48:03 +0800

    Added functions

--- Future/CommonFunctions.sol
+++ Future/CommonFunctions.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.19;
 pragma abicoder v1;
 
-contract ReadOnly {
+library lib {
     function getSelector(string memory a) external pure returns(bytes4) {
         return bytes4(keccak256(abi.encodePacked(a)));
     }
@@ -11,18 +11,28 @@ contract ReadOnly {
         return keccak256(abi.encodePacked(a));
     }
 
-    function hex2Uint(bytes memory a) external pure returns(uint b) {
+    function getContractLength(address a) external view returns(uint) {
+        return a.code.length;
+    }
+
+    function convertHex2Dec(bytes memory a) external pure returns(uint b) {
         for(uint i = 0; i < a.length; ++i) b += uint(uint8(a[i]))*(2**(8*(a.length-i-1)));
     }
 
-    function uint2Hex(uint a) external pure returns (bytes memory b) {
+    function convertDec2Hex(uint a) external pure returns (bytes memory b) {
         uint len;
         for (uint i = a; i > 0; i /= 256) ++len;
         b = new bytes(len);
         for (uint i = 0; i < len; ++i) b[len-i-1] = bytes1(uint8(a / (2**(8*i))));
     }
 
-    function recover(bytes32 h, bytes memory c) external pure returns (address) {
+    function convertStr2Hex(string memory s) external pure returns(bytes32 b) {
+        assembly {
+            b := mload(add(s, 0x20))
+        }
+    }
+
+    function recover(bytes32 h, bytes memory c) external pure returns(address) {
         uint8 v;
         bytes32 r;
         bytes32 s;
@@ -34,7 +44,7 @@ contract ReadOnly {
         return ecrecover(h, v, r, s);
     }
 
-    function recover(bytes32 h, uint8 v, bytes32 r, bytes32 s) external pure returns (address) {
+    function recover(bytes32 h, uint8 v, bytes32 r, bytes32 s) external pure returns(address) {
         return ecrecover(h, v, r, s);
     }
 }
