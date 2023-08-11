e5f2095d1ea03920d1eef5320be83e5ea8f10a9d
aloycwl <aloycwl@gmail.com>
2023-08-11 10:21:35 +0800

    Created CIDDB

---
+++ Future/CIDDB.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: None
+pragma solidity 0.8.19;
+pragma abicoder v1;
+
+/*
+A database to store the CID that could have been reused and
+wasted the total storage space. This will be more effective
+than ERC-1155 if the CID of the metadate is the same. Instead
+of using at least 3 memory lines for string, CID database only
+need 1 storage slot to fetch the CID or URL.
+
+Store the CID with a return uint value if it does not exist.
+The uint value will be used to call the fetch() function
+which will return the string.
+*/
+
+contract CIDDB {
+    mapping(string => uint) public search;
+    mapping(uint => string) public fetch;
+
+    function setFetch(string memory cid) external returns(uint nwc) {
+        require(CIDDB(address(this)).search(cid) == 0, "CID Existed");
+        assembly {
+            nwc := add(sload(0x00), 1)
+            sstore(0x00, nwc)
+        }
+        search[cid] = nwc;
+        fetch[nwc] = cid;
+    }
+
+    function count() external view returns(uint) {
+        assembly {
+            mstore(0x00, sload(0x00))
+            return(0x00, 0x20)
+        }
+    }
+}
+
